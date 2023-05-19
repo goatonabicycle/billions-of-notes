2833700a26eb90cf23f6c76596bc604c9613fcae
Rowan <malice.peanutbutter@gmail.com>
2023-05-19 22:31:59 +0200

    Better defaults

--- src/useful.js
+++ src/useful.js
@@ -1,8 +1,9 @@
 // utils.js
 export const DEFAULT_KEY = "C";
 export const DEFAULT_SCALE = "ionian";
-export const DEFAULT_TEMPO = 120;
-export const DEFAULT_NUMBER_OF_NOTES = 4;
+export const DEFAULT_TEMPO = 220;
+export const DEFAULT_NUMBER_OF_NOTES = 8;
+export const DEFAULT_OCTAVE = 3;
 
 export const KEYS = [
   "A",
