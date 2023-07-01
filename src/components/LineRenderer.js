a1cc3f77afbceb60063c1c2050d3077124d2d252
Rowan <malice.peanutbutter@gmail.com>
2023-07-01 17:38:49 +0200

    LineRenderer now works with empty notes.

--- src/components/LineRenderer.js
+++ src/components/LineRenderer.js
@@ -10,6 +10,8 @@ const LINE_COLOUR = "white";
 const SECONDARY_LINE_COLOUR = "rgba(121, 75, 196, 0.5)";
 
 const getNoteNumber = (note) => {
+  if (note === "") return null;
+
   const octave = parseInt(note.slice(-1)) + 1;
   const noteName = note.slice(0, -1);
   return octave * 12 + KEYS.indexOf(noteName);
@@ -20,8 +22,10 @@ const LineRenderer = ({ notes, onClick, activeNote }) => {
 
   const calculateLinePath = () => {
     const noteNumbers = notes.map(getNoteNumber);
-    const minNote = Math.min(...noteNumbers);
-    const maxNote = Math.max(...noteNumbers);
+    const validNoteNumbers = noteNumbers.filter((note) => note !== null);
+
+    const minNote = Math.min(...validNoteNumbers);
+    const maxNote = Math.max(...validNoteNumbers);
     const noteRange = maxNote - minNote;
     const canvas = canvasRef.current;
 
@@ -29,41 +33,47 @@ const LineRenderer = ({ notes, onClick, activeNote }) => {
       x:
         (i / (notes.length - 1)) * (canvas.width - 2 * DOT_RADIUS) + DOT_RADIUS,
       y:
-        ((noteNumber - minNote) / noteRange) *
-          (canvas.height - 2 * DOT_RADIUS) +
-        DOT_RADIUS,
+        noteNumber !== null
+          ? ((noteNumber - minNote) / noteRange) *
+              (canvas.height - 2 * DOT_RADIUS) +
+            DOT_RADIUS
+          : null,
     }));
   };
 
   const drawLine = (linePath, ctx) => {
     ctx.beginPath();
     linePath.forEach((point, i) => {
-      if (i === 0) ctx.moveTo(point.x, point.y);
-      else ctx.lineTo(point.x, point.y);
+      if (point.y !== null) {
+        if (i === 0) ctx.moveTo(point.x, point.y);
+        else ctx.lineTo(point.x, point.y);
 
-      ctx.arc(point.x, point.y, EDGE_DOT_RADIUS, 0, 2 * Math.PI);
-      ctx.fillStyle = FILL_COLOUR;
-      ctx.fill();
+        ctx.arc(point.x, point.y, EDGE_DOT_RADIUS, 0, 2 * Math.PI);
+        ctx.fillStyle = FILL_COLOUR;
+        ctx.fill();
+      }
     });
 
     // Go back to the first point
-    if (notes.length <= 6) ctx.lineTo(linePath[0].x, linePath[0].y);
+    if (linePath.filter((point) => point.y !== null).length <= 6)
+      ctx.lineTo(linePath[0].x, linePath[0].y);
     ctx.strokeStyle = LINE_COLOUR;
     ctx.stroke();
   };
 
   const drawSecondaryLines = (linePath, ctx) => {
     ctx.strokeStyle = SECONDARY_LINE_COLOUR;
-    linePath.slice(1).forEach((point) => {
+    const validPoints = linePath.filter((point) => point.y !== null);
+    validPoints.slice(1).forEach((point) => {
       ctx.beginPath();
-      ctx.moveTo(linePath[0].x, linePath[0].y);
+      ctx.moveTo(validPoints[0].x, validPoints[0].y);
       ctx.lineTo(point.x, point.y);
       ctx.stroke();
     });
   };
 
   const drawAnimationDot = (dotPosition, ctx) => {
-    if (dotPosition) {
+    if (dotPosition && dotPosition.y !== null) {
       ctx.beginPath();
       ctx.arc(dotPosition.x, dotPosition.y, DOT_RADIUS, 0, 2 * Math.PI);
       ctx.fillStyle = ANIMATION_DOT_COLOUR;
