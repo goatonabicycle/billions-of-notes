31d8b045b41cad377b7f20c5abd8fcd3903771ad
Rowan <malice.peanutbutter@gmail.com>
2023-10-12 19:26:45 +0200

    Use sharps!

--- src/WhatScale.js
+++ src/WhatScale.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Note, Scale } from "tonal";
+
+import { FLAT_TO_SHARP } from "./useful";
 import "./WhatScale.css";
 
 function WhatScale() {
@@ -9,7 +11,8 @@ function WhatScale() {
   const handleInputChange = (e) => {
     const inputNotes = e.target.value
       .split(",")
-      .map((note) => Note.simplify(note.trim()));
+      .map((note) => convertToSharp(note.trim()));
+
     setNotes(e.target.value);
     const scales = Scale.detect(inputNotes).filter(
       (scaleName) => !scaleName.toLowerCase().includes("chromatic")
@@ -18,19 +21,25 @@ function WhatScale() {
     setDetectedScales(scales);
   };
 
+  const convertToSharp = (note) => {
+    const { pc, oct } = Note.get(Note.simplify(note));
+    return (FLAT_TO_SHARP[pc] || pc) + (oct || "");
+  };
+
   const renderScaleNotes = (scaleNotes, inputNotes) => {
     return scaleNotes
       .map((scaleNote) => {
-        if (inputNotes.includes(scaleNote)) {
+        const sharpNote = convertToSharp(scaleNote);
+        if (inputNotes.includes(sharpNote)) {
           return (
             <span
               className="containing"
-              key={scaleNote}>
-              {scaleNote}
+              key={sharpNote}>
+              {sharpNote}
             </span>
           );
         } else {
-          return <span key={scaleNote}>{scaleNote}</span>;
+          return <span key={sharpNote}>{sharpNote}</span>;
         }
       })
       .reduce((prev, curr) => [prev, ", ", curr]);
@@ -41,7 +50,8 @@ function WhatScale() {
       <h2>What is this scale?</h2>
       <p class="what-scale-explain">
         Input notes below (separated by commas). The <strong>first note</strong>{" "}
-        you enter is considered the <strong>tonic</strong> of the scale.
+        you enter is considered the <strong>tonic</strong> of the scale. Use
+        sharps (<code>C#</code> instead of <code>Db</code>)
       </p>
       <input
         className="input-box"
