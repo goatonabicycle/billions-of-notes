c88c5aa84903b346f6646f9aba15829b0345bc04
Rowan <malice.peanutbutter@gmail.com>
2023-11-21 20:05:50 +0200

    Piano notes now visible. Pretty cool.

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,13 +1,41 @@
-import React from "react";
-
+import React, { useRef, useEffect, useState } from "react";
 import { Piano as ReactPiano, MidiNumbers } from "react-piano";
 import "react-piano/dist/styles.css";
-
 import "./Piano.css";
 
 const Piano = ({ notesToPlay, playbackIndex, scaleNotes }) => {
-  const currentNote = notesToPlay[playbackIndex];
+  const renderNoteLabel = ({ midiNumber }) => {
+    const { note, octave } = MidiNumbers.getAttributes(midiNumber);
+    const noteLabel = note + octave;
 
+    // Fixes a weird annoying response making the octave add an extra number.
+    const correctNoteLabel =
+      noteLabel.length > 3
+        ? noteLabel.substring(0, 3)
+        : noteLabel.substring(0, 2);
+
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          pointerEvents: "none",
+          width: "100%",
+          marginTop: "10px",
+          color: "black",
+          fontSize: "0.75rem",
+          lineHeight: "20px",
+        }}>
+        {correctNoteLabel}
+      </div>
+    );
+  };
+
+  const noteRange = {
+    first: MidiNumbers.fromNote("c1"),
+    last: MidiNumbers.fromNote("c6"),
+  };
+
+  const currentNote = notesToPlay[playbackIndex];
   let activeNote;
   if (currentNote) {
     activeNote = MidiNumbers.fromNote(currentNote);
@@ -15,16 +43,13 @@ const Piano = ({ notesToPlay, playbackIndex, scaleNotes }) => {
 
   return (
     <div className="doodle-border">
-      {"Piano"}
       <div className="piano-container">
         <ReactPiano
-          noteRange={{
-            first: MidiNumbers.fromNote("c1"),
-            last: MidiNumbers.fromNote("c6"),
-          }}
+          noteRange={noteRange}
           playNote={() => {}}
           stopNote={() => {}}
           activeNotes={[activeNote]}
+          renderNoteLabel={renderNoteLabel}
         />
       </div>
     </div>
