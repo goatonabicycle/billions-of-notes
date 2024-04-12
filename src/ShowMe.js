566ca7b1570c17257eb1c89d1bb5387ce2a0dbe1
Rowan <malice.peanutbutter@gmail.com>
2024-04-12 20:59:10 +0200

    Show me improvements. Showing notes from a file.

--- src/ShowMe.js
+++ src/ShowMe.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Guitar from "./components/Guitar";
+import { Midi } from "@tonejs/midi";
 
 function ShowMe() {
+  const [midiFile, setMidiFile] = useState(null);
+  const [notes, setNotes] = useState([]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setMidiFile(file);
+      readMidiFile(file);
+    }
+  };
+
+  const readMidiFile = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = e.target.result;
+      parseMidi(data);
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  const parseMidi = (data) => {
+    const midi = new Midi(data);
+    const notes = midi.tracks.flatMap((track) =>
+      track.notes.map((note) => `${note.name}${note.octave}`)
+    );
+    setNotes(notes);
+  };
+
   return (
     <>
       <h1>Show Me Page</h1>
-      There will be a file input here that allows you to import a .midi file.
-      Having an alternative way to allow users to make up a song might be nice
-      as well.
+      <input
+        type="file"
+        onChange={handleFileChange}
+        accept=".midi,.mid"
+      />
+      <div>
+        <strong>Notes:</strong>
+        {notes.map((note, index) => (
+          <div key={index}>{note}</div>
+        ))}
+      </div>
       <Guitar
         playbackIndex={0}
-        notesToPlay={["a3"]}
+        notesToPlay={notes}
         scaleNotes={[]}
       />
     </>
