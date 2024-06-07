649b9778920d6bc810d75e52c1c0f0a506909f2e
Rowan <malice.peanutbutter@gmail.com>
2024-06-07 20:06:43 +0200

    fix: output to "build" for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig((configEnv) => {
     server: {
       port: 3000,
     },
+    build: {
+      outDir: "build",
+    },
     test: {
       globals: true,
       environment: "happy-dom",
