13e4f15c1f424ed8a673adc96f4af7f4eb617af1
Rowan <malice.peanutbutter@gmail.com>
2024-10-24 23:01:43 +0200

    Next to each other

--- src/CoolAmpwallStuff.js
+++ src/CoolAmpwallStuff.js
@@ -2,29 +2,62 @@ import React, { useState } from "react";
 
 function Ampwall() {
 	return (
-		<div className="container">
-			<h2>llawpma</h2>
-			Should not work:
-			<iframe
-				src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
+		<div
+			style={{
+				maxWidth: "1200px",
+				margin: "0 auto",
+				padding: "20px",
+			}}
+		>
+			<h2 style={{ marginBottom: "20px" }}>llawpma</h2>
+
+			<div
 				style={{
-					width: "350px",
-					minWidth: "350px",
-					height: "501px",
-					minHeight: "492px",
+					display: "flex",
+					flexDirection: "row",
+					gap: "30px",
+					justifyContent: "center",
+					flexWrap: "wrap",
 				}}
-			></iframe>
-			<br />
-			Should work:
-			<iframe
-				src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c051-6f2b-7f70-85b9-59f6900c3038&controlStyle=persistent&playerWidth=350"
-				style={{
-					width: "350px",
-					minWidth: "350px",
-					height: "501px",
-					minHeight: "492px",
-				}}
-			></iframe>
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<p style={{ marginBottom: "10px" }}>Should not work:</p>
+					<iframe
+						src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c035-5f68-7650-a225-97db82245d75&controlStyle=persistent&playerWidth=350"
+						style={{
+							width: "350px",
+							minWidth: "350px",
+							height: "501px",
+							minHeight: "492px",
+						}}
+					></iframe>
+				</div>
+
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<p style={{ marginBottom: "10px" }}>Should work:</p>
+					<iframe
+						src="https://ampwall.com/services/PlayerCard/v1/content/exclusive?embedId=0192c051-6f2b-7f70-85b9-59f6900c3038&controlStyle=persistent&playerWidth=350"
+						style={{
+							width: "350px",
+							minWidth: "350px",
+							height: "501px",
+							minHeight: "492px",
+						}}
+					></iframe>
+				</div>
+			</div>
 		</div>
 	);
 }
